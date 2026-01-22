8ddf045cfef9bd52e14f5bb6c1b83345f835c5d7
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-01-22 21:47:37 +0530

    basics of python

---
+++ Data.java
@@ -0,0 +1,8 @@
+//Write a program to store your name, age, and marks and print them.
+
+name =input("ENTER YOUR NAME:")
+age = input("ENTER YOUR AGE:")
+marks= input("ENTER YOUR MARKS")
+print("my name is",name)
+print("my age is",age)
+print("my marks are",marks)
