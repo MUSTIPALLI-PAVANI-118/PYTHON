1edebad7d5558112a543908a4c680ba933264395
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-01-23 13:47:20 +0530

    Checking whether the entered num is positive ,negative or zero

---
+++ number.py
@@ -0,0 +1,8 @@
+#Write a program to check whether a number is positive, negative, or zero.
+num=int(input("ENTER THE NUM:"))
+if num > 0:
+    print("ENTERED NUM IS POSITIVE")
+elif num < 0:
+    print("ENTERED NUM IS NEGATIVE")
+else:
+    print("ENTERED NUM IS ZER0")
