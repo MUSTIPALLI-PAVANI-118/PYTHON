32f6b38720dd0d2477152bfb3ce73f0832cd534d
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-01-23 17:33:37 +0530

    checking largest of two numbers

---
+++ Largestnum.py
@@ -0,0 +1,7 @@
+#Write a program to find the largest of two numbers.
+num1=int(input("ENTER THE NUM1:"))
+num2=int(input("ENTER THE NUM2:"))
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
