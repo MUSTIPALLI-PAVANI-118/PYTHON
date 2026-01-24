b3ba10e66d9f59c302ace9e6866341d608ff2cba
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-01-24 19:03:25 +0530

    using operators

---
+++ num.py
@@ -0,0 +1,11 @@
+num1=int(input("enter num1 value:"))
+num2=int(input("enter num2 value:"))
+#printing operators
+sum=num1+num2
+division =num1/num2
+multiplication = num1*num2
+subtraction=num1-num2
+print("SUM OF NUMBERS",sum)
+print("SUBTRACTION OF NUMBERS",subtraction)
+print("MULTIPLICATION OF NUMBERS",multiplication)
+print("DIVISION OF NUMBERS",division)
