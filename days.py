10d7253abe93cfe5c8799199c9cf7d77f3319e36
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-01-24 19:10:53 +0530

    PRINTING DAYS USING IF ELSE CASES

---
+++ days.py
@@ -0,0 +1,18 @@
+num=int(input("enter the num value(0-7)"))
+if num ==1:
+    print("MONDAY")
+elif num ==2:
+    print("TUESDAY")
+elif num ==3:
+    print("WEDNESDAY")
+elif num ==4:
+    print("THURSDAY")
+elif num ==5:
+    print("FRIDAY")
+elif num ==6:
+    print("SATURDAY")
+elif num ==7:
+    print("SUNDAY")
+else:
+    print("WRONG INPUT")
+  
