22a61e2f2b297e4fea0f8d423db7ab56091ff382
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-01-25 09:14:25 +0530

    CONVERTING KILOMETERS TO METERS

---
+++ convert.py
@@ -0,0 +1,5 @@
+#CONVERTING KILOMETERS TO METERS
+kilometers = int(input("ENTER THE VALUE OF KILOMETER:"))
+#CONVERTING TO METERS
+meters=((18/5)*kilometers)
+print("The distance in meter is ",meters)
