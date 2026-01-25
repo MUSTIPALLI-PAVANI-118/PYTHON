ad66d38bec024c95f5c108d52994a920b6dc9100
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-01-25 09:00:53 +0530

    Swaping of  two numbers using a temporary variable

---
+++ Swap.py
@@ -0,0 +1,8 @@
+#Swap two numbers using a temporary variable
+num1=int(input("ENTER THE NUM1 VALUE:"))
+num2=int(input("ENTER THE NUM2 VALUE:"))
+num3=num2
+num4=num1
+print("Before swapping numbers are " ,num1 ,"and ",num2 )
+print("After swapping of numbers ",num1 ,"is ",num3 )
+print("After swapping of numbers ",num2 ,"is ",num4 )
