3a8670ba0778e88ef5d899c086d8514423efa47a
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-01-29 14:33:28 +0530

    swapping numbers without using third variable

---
+++ Swapnum.py
@@ -0,0 +1,7 @@
+#swapping numbers without using third variable
+num1 = int(input("ENTER THE NUM1:")
+num2 = int(input("ENTER THE SUM2:")
+num1 = num1 + num2
+num2 = num1 - num2 
+num1 = num1 - num2
+print("numbers after swapping are:" , num1, " and ",num2)
