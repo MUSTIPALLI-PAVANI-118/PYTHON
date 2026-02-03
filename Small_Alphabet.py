211615a5a2fdf6eb298dbb49043e7a7f5639918a
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-02-03 10:47:30 +0530

    CHECKING WHETHER THE SMALL ALPHABETS IS VOWEL OR CONSONANTS

---
+++ Small_Alphabet.py
@@ -0,0 +1,13 @@
+alphabet =  input("ENTER THE ALPHABET:")
+if alphabet="a":
+  print("entered alphabet is vowel")
+elif alphabet="e":
+  print("entered alphabet is vowel")
+elif alphabet="i":
+  print("entered alphabet is vowel")
+elif alphabet="o":
+  print("entered alphabet is vowel")
+elif alphabet = "u"):
+  print("entered alphabet is vowel")
+else:
+  print("entered alphabet is consonant") 
