b800c8ce057702fd94c0afbcd16ae77be0811a03
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-02-03 11:24:29 +0530

    CHECKING WHETHER THE UPPER CASE ALPHABET IS VOWEL OR CONSONANT

---
+++ Upper_Alphabet.py
@@ -0,0 +1,13 @@
+alphabet =  input("ENTER THE ALPHABET:")
+if alphabet=A:
+  print("entered alphabet is vowel")
+elif alphabet="E":
+  print("entered alphabet is vowel")
+elif alphabet="I":
+  print("entered alphabet is vowel")
+elif alphabet="O":
+  print("entered alphabet is vowel")
+elif alphabet = "U":
+  print("entered alphabet is vowel")
+else:
+  print("entered alphabet is consonant")
