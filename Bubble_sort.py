2ae647062dbbd6b4373931d9970a4ebc50797fef
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-02-16 10:30:49 +0530

    BUBBLE SORT ALGORITHM

---
+++ Bubble_sort.py
@@ -0,0 +1,10 @@
+ef bubble_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+arr=[6,4,3,2,5,1]
+print(bubble_sort(arr))
