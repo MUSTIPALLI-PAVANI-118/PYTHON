127484b248842dba69e6da0b03f5f28527d5b62e
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-03-20 14:00:54 +0530

    second_largest

---
+++ second_largest.py
@@ -0,0 +1,15 @@
+# second_largest.py
+
+arr = [10, 20, 4, 45, 99]
+
+largest = float('-inf')
+second_largest = float('-inf')
+
+for num in arr:
+    if num > largest:
+        second_largest = largest
+        largest = num
+    elif num > second_largest and num != largest:
+        second_largest = num
+
+print("Second Largest Element:", second_largest)
